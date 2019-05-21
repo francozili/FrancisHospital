90b2404be269670211bda687f9455ab6d79ef43d
francis_chukwuma <francischukwuma@PSADMINs-MacBook-Air.local>
2019-05-21 12:53:25 -0400

    added proxy to package.json

--- server.js
+++ server.js
@@ -10,12 +10,9 @@ app.use(express.static(`${__dirname}/client/build`))
 
 app.use('/api/v1', routes)
 
-// app.get('/*', (req, res) => {
-//     res.sendFile(`${__dirname}/client/build/index.html`)
-// })
-app.get('/', (req, res) => {
-    res.send("Hello World")
-  })
+app.get('/*', (req, res) => {
+    res.sendFile(`${__dirname}/client/build/index.html`)
+})
 const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
