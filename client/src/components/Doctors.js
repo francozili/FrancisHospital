df62bdc7d8f67a175f5148a1bcdf42833e13ad28
francis_chukwuma <francischukwuma@PSADMINs-Air.attlocal.net>
2019-05-22 01:42:22 -0400

    finished patient component

--- client/src/components/Doctors.js
+++ client/src/components/Doctors.js
@@ -3,124 +3,126 @@ import axios from 'axios'
 import { Link } from 'react-router-dom'
 
 class Doctors extends Component {
-  state = {
-      doctors: [],
-      newDoctor: {
-        name: '',
-        picture: '',
-        address: '',
-        phone: '',
-        specialty: ''
-      },
-      isDoctorFormDisplayed: false
-  }
+    state = {
+        doctors: [],
+        newDoctor: {
+            name: '',
+            picture: '',
+            address: '',
+            phone: '',
+            specialty: ''
+        },
+        isDoctorFormDisplayed: false
+    }
 
-  componentDidMount = () => {
-    axios.get('/api/v1/doctors').then(res => {
-        this.setState({doctors: res.data})
-    })
-  }
-
-  toggleDoctorForm = () => {
-      this.setState((state, props) => {
-          return ({isDoctorFormDisplayed: !state.isDoctorFormDisplayed})
-      })
-  }
-
-  
-  handleChange = (e) => {
-    const cloneNewDoctor = {...this.state.newDoctor}
-    cloneNewDoctor[e.target.name] = e.target.value
-    this.setState({newDoctor: cloneNewDoctor},()=>{console.log(this.state.newDoctor)})
-  }
-
-  createDoctor = (e) => {
-    e.preventDefault()
-    console.log(this.state.newDoctor.name)
-    axios
-        .post('/api/v1/doctors', {
-            name: this.state.newDoctor.name,
-            description: this.state.newDoctor.description
+    componentDidMount = () => {
+        axios.get('/api/v1/doctors').then(res => {
+            this.setState({ doctors: res.data })
         })
-        .then(res => {
-            const doctorsList = [...this.state.doctors]
-            doctorsList.unshift(res.data)
-            this.setState({
-                newDoctor: {
-                    name: '',
-                    picture: '',
-                    address: '',
-                    phone: '',
-                    specialty: ''
-                },
-                isDoctorFormDisplayed: false,
-                doctors: doctorsList
-            })
+    }
+
+    toggleDoctorForm = () => {
+        this.setState((state, props) => {
+            return ({ isDoctorFormDisplayed: !state.isDoctorFormDisplayed })
         })
+    }
 
-  }
 
-  render() {
-    return (
-      <div>
-        <h1>Doctors</h1>
-        {
-            this.state.doctors.map(doctor => {
-                return (
-                    <div key={doctor._id}>
-                        <Link
-                            to={`/doctors/${doctor._id}`}
-                        >
-                            {doctor.name}
-                        </Link>
-                    </div>
-                )
+    handleChange = (e) => {
+        const cloneNewDoctor = { ...this.state.newDoctor }
+        cloneNewDoctor[e.target.name] = e.target.value
+        this.setState({ newDoctor: cloneNewDoctor }, () => { console.log(this.state.newDoctor) })
+    }
+
+    createDoctor = (e) => {
+        e.preventDefault()
+        console.log(this.state.newDoctor.name)
+        axios
+            .post('/api/v1/doctors', {
+                name: this.state.newDoctor.name,
+                description: this.state.newDoctor.description
             })
-        }
-        <button onClick={this.toggleDoctorForm}>+ New Doctor</button>
-        {
-            this.state.isDoctorFormDisplayed
-                ? <form onSubmit={this.createDoctor}>
-                    <div className="form-group">
-                        <label htmlFor="name">Name</label>
-                        <input type="text"
-                            name="name"
-                            className="form-control"
-                            onChange={this.handleChange} />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="name">Picture</label>
-                        <input type="text"
-                            name="picture"
-                            className="form-control"
-                            onChange={this.handleChange} />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="name">Address</label>
-                        <input type="text"
-                            className="form-control"
-                            name="address"
-                            onChange={this.handleChange} />
-                    </div>
-                    <label htmlFor="name">Phone</label>
-                    <input type="text"
-                        className="form-control"
-                        name="phone"
-                        onChange={this.handleChange} />
-                    <div className="form-group">
-                        <label htmlFor="name">Specialty</label>
-                        <input type="text"
-                            className="form-control"
-                            name="specialty"
-                            onChange={this.handleChange} />
-                    </div>
-                    <button>Create</button>
-                </form>
-                : null
-        }
-      </div>
-    )
-  }
+            .then(res => {
+                const doctorsList = [...this.state.doctors]
+                doctorsList.unshift(res.data)
+                this.setState({
+                    newDoctor: {
+                        name: '',
+                        picture: '',
+                        address: '',
+                        phone: '',
+                        specialty: ''
+                    },
+                    isDoctorFormDisplayed: false,
+                    doctors: doctorsList
+                })
+            })
+
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Doctors</h1>
+                {
+                    this.state.doctors.map(doctor => {
+                        return (
+                            <div key={doctor._id}>
+                                <Link
+                                    to={`/doctors/${doctor._id}`}
+                                >
+                                    {doctor.name}
+                                </Link>
+                            </div>
+                        )
+                    })
+                }
+                <button onClick={this.toggleDoctorForm}>+ New Doctor</button>
+                {
+                    this.state.isDoctorFormDisplayed
+                        ? <form onSubmit={this.createDoctor}>
+                            <div className="form-group">
+                                <label htmlFor="name">Name</label>
+                                <input type="text"
+                                    name="name"
+                                    className="form-control"
+                                    onChange={this.handleChange} />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="name">Picture</label>
+                                <input type="text"
+                                    name="picture"
+                                    className="form-control"
+                                    onChange={this.handleChange} />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="name">Address</label>
+                                <input type="text"
+                                    className="form-control"
+                                    name="address"
+                                    onChange={this.handleChange} />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="name">Phone</label>
+                                <input type="text"
+                                    className="form-control"
+                                    name="phone"
+                                    onChange={this.handleChange} />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="name">Specialty</label>
+                                <input type="text"
+                                    className="form-control"
+                                    name="specialty"
+                                    onChange={this.handleChange} />
+                            </div>
+                            <button>Create</button>
+                        </form>
+                        : null
+                }
+            </div>
+        )
+    }
 }
 
 export default Doctors
--- client/src/components/Patients.js
+++ client/src/components/Patients.js
@@ -3,126 +3,135 @@ import axios from 'axios'
 import { Link } from 'react-router-dom'
 
 class Patients extends Component {
-  state = {
-    patients: [],
-      newPatient: {
-        name: '',
-        age: '',
-        appointment: '',
-        address: '',
-        phone: '',
-        Doctor: []
-      },
-      isPatientFormDisplayed: false
-  }
+    state = {
+        patients: [],
+        newPatient: {
+            name: '',
+            age: '',
+            appointment: '',
+            address: '',
+            phone: '',
+            doctor: []
+        },
+        isPatientFormDisplayed: false
+    }
 
-  componentDidMount = () => {
-    axios.get('/api/v1/patients').then(res => {
-        this.setState({patients: res.data})
-    })
-  }
-
-  toggleDoctorForm = () => {
-      this.setState((state, props) => {
-          return ({isPatientFormDisplayed: !state.isPatientFormDisplayed})
-      })
-  }
-
-  
-  handleChange = (e) => {
-    const cloneNewPatient = {...this.state.newPatient}
-    cloneNewPatient[e.target.name] = e.target.value
-    this.setState({newPatient: cloneNewPatient},()=>{console.log(this.state.newPatient)})
-  }
-
-  createPatients = (e) => {
-    e.preventDefault()
-    console.log(this.state.newPatient.name)
-    axios
-        .post('/api/v1/patients', {
-            name: this.state.newPatient.name,
-            description: this.state.newPatient.description
+    componentDidMount = () => {
+        axios.get('/api/v1/patients').then(res => {
+            this.setState({ patients: res.data })
         })
-        .then(res => {
-            const patientsList = [...this.state.patients]
-            patientsList.unshift(res.data)
-            this.setState({
-                newPatient: {
-                    name: '',
-                    age: '',
-                    appointment: '',
-                    address: '',
-                    phone: '',
-                    Doctor: []
-                },
-                isPatientFormDisplayed: false,
-                patients: patientsList
-            })
+    }
+
+    toggleDoctorForm = () => {
+        this.setState((state, props) => {
+            return ({ isPatientFormDisplayed: !state.isPatientFormDisplayed })
         })
+    }
 
-  }
 
-  render() {
-    return (
-      <div>
-        <h1>Patients</h1>
-        {
-            this.state.patients.map(patient => {
-                return (
-                    <div key={patient._id}>
-                        <Link
-                            to={`/patients/${patient._id}`}
-                        >
-                            {patient.name}
-                        </Link>
-                    </div>
-                )
+    handleChange = (e) => {
+        const cloneNewPatient = { ...this.state.newPatient }
+        cloneNewPatient[e.target.name] = e.target.value
+        this.setState({ newPatient: cloneNewPatient }, () => { console.log(this.state.newPatient) })
+    }
+
+    createPatients = (e) => {
+        e.preventDefault()
+        console.log(this.state.newPatient.name)
+        axios
+            .post('/api/v1/patients', {
+                name: this.state.newPatient.name,
+                description: this.state.newPatient.description
             })
-        }
-        <button onClick={this.togglePatientForm}>+ New Patient</button>
-        {
-            this.state.isPatientFormDisplayed
-                ? <form onSubmit={this.createDoctor}>
-                    <div className="form-group">
-                        <label htmlFor="name">Name</label>
-                        <input type="text"
-                            name="name"
-                            className="form-control"
-                            onChange={this.handleChange} />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="name">Picture</label>
-                        <input type="text"
-                            name="picture"
-                            className="form-control"
-                            onChange={this.handleChange} />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="name">Address</label>
-                        <input type="text"
-                            className="form-control"
-                            name="address"
-                            onChange={this.handleChange} />
-                    </div>
-                    <label htmlFor="name">Phone</label>
-                    <input type="text"
-                        className="form-control"
-                        name="phone"
-                        onChange={this.handleChange} />
-                    <div className="form-group">
-                        <label htmlFor="name">Specialty</label>
-                        <input type="text"
-                            className="form-control"
-                            name="specialty"
-                            onChange={this.handleChange} />
-                    </div>
-                    <button>Create</button>
-                </form>
-                : null
-        }
-      </div>
-    )
-  }
+            .then(res => {
+                const patientsList = [...this.state.patients]
+                patientsList.unshift(res.data)
+                this.setState({
+                    newPatient: {
+                        name: '',
+                        age: '',
+                        appointment: '',
+                        address: '',
+                        phone: '',
+                        Doctor: []
+                    },
+                    isPatientFormDisplayed: false,
+                    patients: patientsList
+                })
+            })
+
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Patients</h1>
+                {
+                    this.state.patients.map(patient => {
+                        return (
+                            <div key={patient._id}>
+                                <Link
+                                    to={`/patients/${patient._id}`}
+                                >
+                                    {patient.name}
+                                </Link>
+                            </div>
+                        )
+                    })
+                }
+                <button onClick={this.togglePatientForm}>+ New Patient</button>
+                {
+                    this.state.isPatientFormDisplayed
+                        ? <form onSubmit={this.createPatient}>
+                            <div className="form-group">
+                                <label htmlFor="name">Name</label>
+                                <input type="text"
+                                    name="name"
+                                    className="form-control"
+                                    onChange={this.handleChange} />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="name">Age</label>
+                                <input type="number"
+                                    name="age"
+                                    className="form-control"
+                                    onChange={this.handleChange} />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="name">Address</label>
+                                <input type="text"
+                                    className="form-control"
+                                    name="address"
+                                    onChange={this.handleChange} />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="name">Phone</label>
+                                <input type="text"
+                                    className="form-control"
+                                    name="phone"
+                                    onChange={this.handleChange} />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="name">Appointment</label>
+                                <input type="text"
+                                    className="form-control"
+                                    name="appointment"
+                                    onChange={this.handleChange} />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="name">Doctor</label>
+                                <input type="text"
+                                    className="form-control"
+                                    name="doctor"
+                                    onChange={this.handleChange} />
+                            </div>
+                            <button>Create</button>
+                        </form>
+                        : null
+                }
+            </div>
+        )
+    }
 }
 
 export default Patients
